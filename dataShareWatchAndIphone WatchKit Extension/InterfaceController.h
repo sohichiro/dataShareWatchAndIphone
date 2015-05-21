22b2c253bf7ade275d5b5a4cbc364075cd1ac6d2
長尾聡一郎 <sohichiro_mini@nagaosouichirou-no-Mac-mini.local>
2015-05-21 10:58:41 +0900

    first commit

---
+++ dataShareWatchAndIphone WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  dataShareWatchAndIphone WatchKit Extension
+//
+//  Created by 長尾聡一郎 on 2015/05/21.
+//  Copyright (c) 2015年 長尾聡一郎. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ dataShareWatchAndIphone WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  dataShareWatchAndIphone WatchKit Extension
+//
+//  Created by 長尾聡一郎 on 2015/05/21.
+//  Copyright (c) 2015年 長尾聡一郎. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
